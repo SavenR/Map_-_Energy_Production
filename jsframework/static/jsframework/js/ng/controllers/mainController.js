0bdf37efed5c0b2598b5cd31ecd3b199fc474081
Saven Roybal <SavenR@users.noreply.github.com>
2015-06-06 18:50:02 -0700

    Disabled API call to silence alerts while developing

--- jsframework/static/jsframework/js/ng/controllers/mainController.js
+++ jsframework/static/jsframework/js/ng/controllers/mainController.js
@@ -2,10 +2,10 @@ app.controller('mainController', function($http){
     var mc = this;
 
     // template API connection to /slugs
-    $http.get('/slugs')
-    .then(function(data){
-        console.log('connected to api')
-        console.log(data);
-    })
+    // $http.get('/slugs')
+    // .then(function(data){
+    //     console.log('connected to api')
+    //     console.log(data);
+    // })
 
 });
